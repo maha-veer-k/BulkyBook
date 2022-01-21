e84c54c3f51c8057cd37a658048cb1bc68679837
Pc <Pc@DESKTOP-IHRQ18L>
2022-01-21 17:55:03 +0530

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;   
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList  = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        public IActionResult CreateCategory()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateCategory(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "DisplayOrder and Name cannot be same");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+
+}
+
